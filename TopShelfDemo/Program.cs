dbc5260317fc3ea5fd4c43a124894020a2465833
Steven Hayes <shayes2@mmm.com>
2020-04-09 12:09:25 -0500

    Pushing up Demo

---
+++ TopShelfDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Topshelf;
+
+namespace TopShelfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig => 
+                            {
+                                serviceConfig.Service<Service>(serviceInstance =>
+                                                               {
+                                                                   serviceInstance.ConstructUsing(
+                                                                       () => new Service());
+
+                                                                   serviceInstance.WhenStarted(execute => execute.Start());
+                                                                   serviceInstance.WhenStopped(execute => execute.Stop());
+                                                               });
+
+                                serviceConfig.SetServiceName("TopShelfDemo");
+                                serviceConfig.SetDisplayName("Top Shelf Demo");
+                                serviceConfig.SetDescription("Top Shelf Demo from Pluralsight");
+
+                                serviceConfig.StartAutomatically();
+                            });
+        }
+    }
+}
---
+++ TopShelfDemo/Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TopShelfDemo
+{
+    class Service
+    {
+        private FileSystemWatcher watcher;
+
+        public bool Start()
+        {
+            watcher = new FileSystemWatcher(@"C:\CodeTesting\TopShelf", "TestingFile.txt");
+
+            watcher.Created += FileCreated;
+
+            watcher.IncludeSubdirectories = true;
+
+            watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            string content = File.ReadAllText(e.FullPath);
+
+            string upperContent = content.ToUpperInvariant();
+
+            var dir = Path.GetDirectoryName(e.FullPath);
+
+            var convertedFileName = Path.GetFileName(e.FullPath).Replace(".txt", "") + "_backup.txt";
+
+            var convertedPath = Path.Combine(dir, convertedFileName);
+
+            File.WriteAllText(convertedPath, upperContent);
+        }
+
+        public bool Stop()
+        {
+            watcher.Dispose();
+
+            return true;
+        }
+    }
+}
